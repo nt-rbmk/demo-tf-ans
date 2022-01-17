3925aa4946a199f2e2c0ae8938e03a1ea46855d4
nt-rbmk <61050727+nt-rbmk@users.noreply.github.com>
2022-01-17 14:06:17 +0200

    Adding S3 preparation using tf

--- pre/main.tf
+++ pre/main.tf
@@ -1,1 +1,22 @@
+variable "region" {
+  default = "eu-central-1"
+}
+locals { environment = "nt-rbmk" }
+
+provider "aws" {
+  region  = var.region
+  profile = local.environment
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "nt-rbmk-terraform-state"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 
